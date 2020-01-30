01d1d63a6890479ae2998206da5757332be6f154
Emma Sofie Skarstein <emma@skarstein.no>
2020-01-30 12:35:01 +0100

    added fish status survey code

---
+++ Fish_status_survey_of_nordic_lakes/R/nordic_lakes_code.R
@@ -0,0 +1,102 @@
+library(ggplot2)
+library(maps)
+library(dplyr)
+
+
+# Load occurrence data and lakes
+occ <- read.table("data/occurrence.txt", header = TRUE, sep = "\t")
+events <- read.table("data/event.txt", header = TRUE, sep = "\t")
+lakes <- readRDS("data/lake_polygons.rds")
+
+colnames(occ)
+colnames(events)
+length(unique(events$eventID))
+length(unique(occ$eventID))
+
+data <- merge(occ, events, by = "eventID")
+
+# Selecting only the variables we need
+year_counts <- count(data, year) %>% filter(year>=1800)
+
+ggplot(year_counts, aes(x = year, y = n)) + 
+  geom_bar(stat = "identity", color="black") + 
+  theme_light() + 
+  ylab("Number of observations")
+
+
+
+# We see that most of the observations are in 1996, so we select only these.
+
+data96 <- data %>% filter(year==1996)
+
+dataNO <- data96 %>% filter(countryCode = "NO")
+dataSE <- data96 %>% filter(countryCode = "SE")
+dataFI <- data96 %>% filter(countryCode = "FI")
+
+test <- data96 %>% filter(grepl('trutta', scientificName))
+length(unique(data96$waterBody))
+##-----------------------------------------------------------------------------------
+## Plot observations
+##-----------------------------------------------------------------------------------
+# Load Norway-map
+map <- map_data("world", region = "Norway(?!:Svalbard)") 
+
+theme_set(theme_light() + theme(aspect.ratio = .70))
+
+ggplot(dataNO) +
+  geom_map(data = map, map = map, aes(long, lat, map_id=region), 
+           color="#2b2b2b", fill = "white") + 
+  geom_point(aes(x = decimalLongitude, y = decimalLatitude, color = occurrenceStatus), 
+             alpha = 0.6, size = 0.5) +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+  guides(colour = guide_legend(override.aes = list(size=2))) 
+
+
+##-----------------------------------------------------------------------------------
+# Match each observation to closest lake
+##-----------------------------------------------------------------------------------
+source("R/match_to_lake.R")
+occ_list <- match_to_lake(dataNO, lakes)
+
+occ_matched <- occ_list[[1]]
+occ_w_lakes <- occ_list[[2]]
+saveRDS(occ_matched, "data/occ_matched.rds")
+saveRDS(occ_w_lakes, "data/occ_w_lakes.rds")
+
+str(occ_matched)
+occ_matched$waterBodyID <- as.factor(occ_matched$waterBodyID)
+
+# How many lakes?
+length(unique(occ_matched$waterBodyID))
+
+##------------------------------------------------------------------------
+trout <- occ_matched %>% filter(grepl('trutta', scientificName))
+
+ggplot(trout) +
+  geom_map(data = map, map = map, aes(long, lat, map_id=region), 
+           color="#2b2b2b", fill = "white") + 
+  geom_point(aes(x = decimalLongitude, y = decimalLatitude, color = occurrenceStatus), 
+             alpha = 0.6, size = 0.5) +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+  guides(colour = guide_legend(override.aes = list(size=2))) 
+
+
+
+##-----------------------------------------------------------------------------------
+# Modelling
+##-----------------------------------------------------------------------------------
+
+model1 <- glm(occurrenceStatus ~ decimalLatitude + decimalLongitude, 
+              family = 'binomial', data = dataNO)
+
+model2 <- glm(occurrenceStatus ~ decimalLongitude + decimalLatitude, 
+              family = 'binomial', data = trout)
+summary(model2)
+
+
+
+# Model fitting in INLA
+library(INLA)
+
+inla_mod <- inla(occurrenceStatus ~ decimalLongitude + decimalLatitude, 
+     family = c('binomial'), data = trout)
---
+++ R/match_to_lake.R
@@ -0,0 +1,66 @@
+library(sf)
+
+#' Match to lake
+#' 
+#' Given a data set of lakefish observations, the function checks if observations are in a lake, and if they are not, matches them to the closest one. (more?)
+#'
+#' @param data The fish observations. 
+#' @param lake_polygons The polygons of the lakes themselves.
+#' @param max_dist_from_lake Maximum tolerated distance from a lake before (what? what do we do with obs far from lakes?)
+#'
+#' @return A new data set containing only observations that are closer than max_dist_from_lake to a lake.
+#' @export
+#'
+#' @examples
+match_to_lake <- function(data, lake_polygons, max_dist_from_lake = 10){
+  #-------------------------------------------------------------------------------------------------
+  # Transforning to sf and selecting some variables
+  #-------------------------------------------------------------------------------------------------
+  message("Transforming data and selecting variables...")
+  data_sf <- data %>% 
+    # Convert to sf object for easier handling. crs = Coordinate Reference System
+    st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326) %>%
+    
+    # Transform coordinate system, using same system as in "lakes"
+    st_transform(st_crs(lake_polygons)$epsg)
+    
+    # Select a few of the variables
+    # dplyr::select(gbifID, occurrenceID, catalogNumber, geometry, species, 
+    #               taxonKey, datasetKey, locality, municipality, county, 
+    #               countryCode, locationID, eventDate, year, month, day, 
+    #               samplingProtocol, eventID, fieldNumber, recordedBy, 
+    #               dynamicProperties, collectionCode, datasetName, license, 
+    #               institutionCode)
+  
+  #-------------------------------------------------------------------------------------------------
+  # Find closest lake
+  #-------------------------------------------------------------------------------------------------
+  message("Joining occurrence data to lake polygons by closest lake...")
+  occ_with_lakes <- st_join(data_sf, lakes, join = st_nearest_feature)
+
+  #-------------------------------------------------------------------------------------------------
+  # Find distance to closest lake
+  #-------------------------------------------------------------------------------------------------
+  message("Calculating distance to closest lake...")
+  index <- st_nearest_feature(x = data_sf, y = lakes) # index of closest lake
+  closest_lakes <- lakes %>% slice(index) # slice based on the index
+  dist_to_lake <- st_distance(x = data_sf, y = closest_lakes, by_element = TRUE) # get distance
+  occ_with_lakes$dist_to_lake <- as.numeric(dist_to_lake) # add the distance calculations to match data
+
+  #-------------------------------------------------------------------------------------------------
+  # Filter out occurrence records not matching lakes (given certain criteria)
+  #-------------------------------------------------------------------------------------------------
+  occ_matched <- occ_with_lakes %>% filter(dist_to_lake < max_dist_from_lake) # 
+  
+  
+  #-------------------------------------------------------------------------------------------------
+  # Looking closer at occurrence records not matching a lake
+  #-------------------------------------------------------------------------------------------------
+  occ_far_from_lake <- occ_with_lakes %>% filter(dist_to_lake > max_dist_from_lake)
+  
+  # Observations outside limit:
+  message("Number of observations further than ", max_dist_from_lake ,"m from a lake: ", nrow(occ_far_from_lake))
+  message("Done! We removed ", round(nrow(occ_far_from_lake)/nrow(occ_with_lakes)*100), "% of the original observations.")
+
+  return(list(occ_matched, occ_with_lakes))
+}

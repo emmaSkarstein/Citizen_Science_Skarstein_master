d48dda9ff7052e004f5c3170eb8e71bc0d8bead6
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-04-15 15:15:00 +0200

    Cleaning up

--- R/preparing_datasets.R
+++ R/preparing_datasets.R
@@ -6,6 +6,7 @@ lakes <- readRDS("data/Norwegian_lakes.rds")
 
 survey_dirty <- readRDS("Fish_status_survey_of_nordic_lakes/data/merged.rds")
 transc_gn_dirty <- readRDS("Transcribed_gillnet_test_fishing_data_norway/data/merged.rds")
+artsobs_dirty <- readRDS("Nordic_Species_Observation_Services/data/GBIF_download_Norge.rds")
 
 survey <- data_prep(survey_dirty, lakes)
 saveRDS(survey, "Fish_status_survey_of_nordic_lakes/data/clean.rds")
@@ -13,6 +14,5 @@ saveRDS(survey, "Fish_status_survey_of_nordic_lakes/data/clean.rds")
 transc_gn <- data_prep(transc_gn_dirty, lakes)
 saveRDS(transc_gn, "Transcribed_gillnet_test_fishing_data_norway/data/clean.rds")
 
-artsobs_dirty <- readRDS("Nordic_Species_Observation_Services/data/GBIF_download_Norge.rds")
 artsobs <- data_prep(artsobs_dirty, lakes, max_dist = 30)                         
-occ_matched <- artsobs_dirty
+saveRDS(artsobs, "Nordic_Species_Observation_Services/data/clean.rds")

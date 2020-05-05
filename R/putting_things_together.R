bffa2c8ae78e495a89a708ae6b88b9f4c411a6c0
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-05-05 21:08:51 +0200

    function to run models for all species plus trout example

--- R/putting_things_together.R
+++ R/putting_things_together.R
@@ -6,29 +6,115 @@ source("R/loading_map_obs_covs.R")
 
 
 
+RunSpeciesModels <- function(species, survey, artsobs){
+  message("Making stack...")
+  stk <- MakeStacks(data_structured = survey, data_unstructured = artsobs,
+                    covariates = Covariates, Mesh = Mesh)
+  
+  # Choose from 
+  # "decimalLatitude", "decimalLongitude",
+  # "area_km2", "perimeter_m", "distance_to_road", 
+  # "eurolst_bio10", "catchment_area_km2", "SCI", "HFP"
+  Use <- c("decimalLongitude","decimalLatitude", "log_area", "perimeter_m", 
+           "eurolst_bio10", "SCI")
+  Use_CS <- c(Use, "distance_to_road", "HFP")
+  
+  formula1 <- MakeFormula(cov_names = Use, second_sp_field = FALSE)
+  formula2 <- MakeFormula(cov_names = Use, second_sp_field = TRUE)
+  formula3 <- MakeFormula(cov_names = Use_CS, second_sp_field = FALSE)
+  formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE)
+  
+  message("Model1...")
+  model1 <- MakeModel(stk, formula1, Mesh)
+  pred1 <- MakePred(stk$pred, model1)
+  
+  message("Model2...")
+  model2 <- MakeModel(stk, formula2, Mesh)
+  pred2 <- MakePred(stk$pred, model2)
+  
+  message("Model3...")
+  model3 <- MakeModel(stk, formula3, Mesh)
+  pred3 <- MakePred(stk$pred, model3)
+  
+  message("Model4...")
+  model4 <- MakeModel(stk, formula4, Mesh)
+  pred4 <- MakePred(stk$pred, model4)
+  
+  return(list(model1 = model1, pred1 = pred1, model2 = model2, pred2 = pred2, 
+              model3 = model3, pred3 = pred3, model4 = model4, pred4 = pred4, 
+              species))
+  
+}
 
-stk <- MakeStacks(data_structured = Data_survey, data_unstructured = Data_artsobs,
-                  covariates = Covariates, Mesh = Mesh)
+pike_models <- RunSpeciesModels("pike", pike_survey, pike_artsobs)
+saveRDS(pike_models, "R/output/pike_models.RDS")
 
-# Split into training and test sets
+trout_models <- RunSpeciesModels("trout", trout_survey, trout_artsobs)
+saveRDS(trout_models, "R/output/trout_models.RDS")
 
-#...
+char_models <- RunSpeciesModels("char", char_survey, char_artsobs)
+saveRDS(char_models, "R/output/char_models.RDS")
+
+perch_models <- RunSpeciesModels("perch", perch_survey, perch_artsobs)
+saveRDS(perch_models, "R/output/perch_models.RDS")
 
 
+plot(pike_models$pred2)
+plot(trout_models$pred2)
+plot(char_models$pred2)
+plot(perch_models$pred2)
 
-# Choose from 
-# "decimalLatitude", "decimalLongitude",
-# "area_km2", "perimeter_m", "distance_to_road", 
-# "eurolst_bio10", "catchment_area_km2", "SCI", "HFP"
+
+PlotSpatialFields(pike_models$model2)
+PlotSpatialFields(trout_models$model2)
+PlotSpatialFields(char_models$model2)
+PlotSpatialFields(perch_models$model2)
+
+
+# Now looking only at trout:
+trout.stk <- MakeStacks(data_structured = trout_survey, data_unstructured = trout_artsobs, 
+                        covariates = Covariates, Mesh = Mesh)
+
 Use <- c("decimalLongitude","decimalLatitude", "log_area", "perimeter_m", 
          "eurolst_bio10", "SCI")
 Use_CS <- c(Use, "distance_to_road", "HFP")
 
-formula1 <- MakeFormula(cov_names = Use, second_sp_field = FALSE)
-formula2 <- MakeFormula(cov_names = Use, second_sp_field = TRUE)
-formula3 <- MakeFormula(cov_names = Use_CS, second_sp_field = FALSE)
-formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE)
+formula5 <- MakeFormula(cov_names = Use, second_sp_field = TRUE, overdispersion = TRUE)
 
-model1_list <- MakeModel(stk, formula1, Mesh)
+trout_mod <- MakeModel(trout.stk, formula5, Mesh)
+trout_pred <- MakePred(trout.stk$pred, trout_mod)
+
+plot(trout_pred)
+PlotSpatialFields(trout_mod, biasfield = TRUE)
 
 
+
+
+
+
+
+
+# Projection <- CRS("+proj=longlat +ellps=WGS84")
+# 
+# Pred <- SpatialPixelsDataFrame(points = stk.pred$predcoords, 
+#                                data = NorwegianModel$predictions, 
+#                                proj4string = Projection)
+# 
+# 
+# boundary <- Mesh$mesh$loc[Mesh$mesh$segm$int$idx[,2],]
+# Nxy.scale <- 0.1 # use this to change the resolution of the predictions
+# Nxy <- round(c(diff(norway.poly@bbox[1,]), diff(norway.poly@bbox[2,]))/Nxy.scale)
+# projgrid <- inla.mesh.projector(Mesh$mesh, xlim=range(boundary[,1]), ylim=range(boundary[,2]), dims=Nxy)
+# xy.in <- splancs::inout(projgrid$lattice$loc, boundary)
+# predcoords <- projgrid$lattice$loc[which(xy.in),]
+# colnames(predcoords) <- c("X", "Y")
+# #Apred <- projgrid$proj$A[which(xy.in), ]
+# 
+# spots <- Mesh$mesh$loc[,1:2]
+# Bias <- SpatialPixelsDataFrame(points = Mesh$mesh$loc[,1:2], 
+#                                data = cbind(model2_list$fitted_model$model$summary.random$bias_field$mean,
+#                                             model2_list$fitted_model$model$summary.random$bias_field$sd), 
+#                                proj4string = Projection)
+# 
+# 
+# 

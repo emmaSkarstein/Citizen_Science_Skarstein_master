82a162822ba240db308e49d5ae0ea395bac1c203
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-06-10 23:16:11 +0200

    final model and other stuff

---
+++ R/compact_results_Emily.R
@@ -0,0 +1,137 @@
+
+# Really compact results for Emily's presentation
+
+
+# LIBRARIES
+library(ggplot)
+library(sf)
+library(sp)
+library(ggpubr)
+library(dplyr)
+library(fishualize)
+library(hexbin)
+library(tidyr)
+library(knitr)
+library(summarytools)
+library(pander)
+library(maps)
+library(maptools)
+library(PointedSDMs)
+library(viridis)
+
+
+# DATA AND MODELS ----
+trout_artsobs <- readRDS("R/output/trout_artsobs.RDS")
+trout_survey <- readRDS("R/output/trout_survey.RDS")
+trout_artsobs_df <- readRDS("R/output/trout_artsobs_df.RDS")
+trout_survey_df <- readRDS("R/output/trout_survey_df.RDS")
+mod <- readRDS("R/output/model_final.RDS")
+dev_list <- readRDS("R/output/cv_output_4modse0.RDS")
+
+
+# VISUALIZE DATA ----
+col_pal <- c('#fff7fb','#ece2f0','#d0d1e6','#a6bddb','#67a9cf','#3690c0','#02818a','#016450')
+
+empty_theme_map <- theme(                              
+  plot.background = element_blank(), 
+  panel.grid.major = element_blank(), 
+  panel.grid.minor = element_blank(), 
+  panel.border = element_blank(), 
+  panel.background = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  axis.title.x = element_blank(), 
+  axis.title.y = element_blank(),
+  axis.text = element_blank()
+)
+
+norway <- ggplot2::map_data("world", region = "Norway(?!:Svalbard)")
+norway <- dplyr::setdiff(norway, filter(norway, subregion == "Jan Mayen"))
+
+artsobs_hex <- ggplot(data.frame(trout_artsobs), aes(x = decimalLongitude, y = decimalLatitude)) +
+  geom_polygon(data = norway, aes(long, lat, group = group), 
+               color='gray93', fill = 'gray93') +
+  geom_hex() +
+  geom_point(aes(x = decimalLongitude, y = decimalLatitude), 
+             alpha = 0.8, size = 1) +
+  scale_fill_gradient(low = "#ecd0d0", high = "#433535") +
+  theme_minimal() +
+  theme(axis.title = element_blank()) +
+  ggtitle("Artsobservasjoner")
+
+survey_hex <- ggplot(data.frame(trout_survey), aes(x = decimalLongitude, y = decimalLatitude)) +
+  geom_polygon(data = norway, aes(long, lat, group = group), 
+               color='gray93', fill = 'gray93') +
+  geom_hex() +
+  geom_point(aes(x = decimalLongitude, y = decimalLatitude, 
+                 shape = occurrenceStatus, size = occurrenceStatus), 
+             alpha = 0.8) +
+  scale_shape_manual(values=c(1, 16), name = "", labels = c("Absence", "Presence")) +
+  scale_size_manual(values = c(2, 1)) +
+  guides(size = FALSE) +
+  scale_fill_gradient(low = "#ecd0d0", high = "#433535") +
+  theme_minimal() +
+  theme(axis.title = element_blank()) +
+  ggtitle("Fish status survey of nordic lakes")
+
+ggarrange(artsobs_hex, survey_hex)
+
+ggsave("figs/hex_point_maps.pdf", width = 8, height = 4)
+
+
+#--------------
+artsobs_hex <- ggplot(data.frame(trout_artsobs), aes(x = decimalLongitude, y = decimalLatitude)) +
+  geom_polygon(data = norway, aes(long, lat, group = group), 
+               color='gray93', fill = 'gray93') +
+  geom_hex() +
+  scale_fill_viridis(option = "magma", direction = -1) +
+  theme_minimal() +
+  theme(axis.title = element_blank()) +
+  ggtitle("Artsobservasjoner (citizen science observations)")
+
+survey_hex <- ggplot(data.frame(trout_survey), aes(x = decimalLongitude, y = decimalLatitude)) +
+  geom_polygon(data = norway, aes(long, lat, group = group), 
+               color='gray93', fill = 'gray93') +
+  geom_hex() +
+  scale_fill_viridis(option = "magma", direction = -1) +
+  theme_minimal() +
+  theme(axis.title = element_blank()) +
+  ggtitle("Fish status survey of nordic lakes")
+
+ggarrange(artsobs_hex, survey_hex)
+ggsave("figs/hex_maps.pdf", width = 12, height = 6)
+
+artsobs_hex
+ggsave("figs/artsobs_hex.pdf", width = 6, height = 6)
+
+# MODEL RESULTS ----
+dev_list %>% unlist() %>% matrix(ncol = 5) %>% rowMeans()
+
+spat_fields_df <- proj_random_field(mod$model, sp_polygon = norway.poly, mesh = Mesh$mesh)
+
+spat_fields <- spat_fields_df %>% gather(key = statistic, value = value, mean:sd)
+
+# New labels for statistics variable
+statistic.labs <- c("Mean", "Standard deviation")
+names(statistic.labs) <- c("mean", "sd")
+
+# New labels for field variable
+field.labs <- c("Second spatial field (PO data only)", "First spatial field (both data sets)")  
+names(field.labs) <- c("bias", "shared")
+
+ggplot(spat_fields) +
+  geom_polygon(data = norway, aes(long, lat, group = group), 
+               color='gray93', fill = 'gray93') +
+  coord_quickmap() + 
+  geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
+  scale_fill_viridis(option = "inferno", direction = -1) +
+  facet_grid(rows = vars(statistic), cols = vars(field),
+             labeller = labeller(field = field.labs, statistic = statistic.labs)) +
+  theme_bw() +
+  theme(axis.title = element_blank()) 
+
+ggsave("figs/spatial_fields.pdf")
+
+summary(mod$model)
+
+
---
+++ R/full_model.R
@@ -0,0 +1,37 @@
+# Final model 
+
+setwd("Citizen_Science_Skarstein_master")
+
+# Loading observations and covariates
+source("R/loading_map_obs_covs.R")
+
+# Loading functions to fit models
+source("R/Model_fitting_functions.R")
+
+# Loading functions to visualize models
+source("R/Model_visualization_functions.R")
+
+message("Making stacks...\n")
+stks <- MakeStacks(data_structured = trout_survey, data_unstructured = trout_artsobs,
+                   env_covariates = env_covariates, all_covariates = Covariates, Mesh = Mesh)
+
+stk.survey <- stks$survey
+stk.artsobs <- stks$artsobs
+stk.ip <- stks$ip
+stk.pred <- stks$pred
+
+Use <- c("decimalLongitude","decimalLatitude", "log_area", "log_perimeter", 
+         "log_catchment", "eurolst_bio10", "SCI")
+Use_CS <- c(Use, "distance_to_road", "HFP")
+
+#formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE, overdispersion = TRUE)
+formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE)
+
+message("Fitting model... \n")
+model_final <- FitModelTest(stk.survey, stk.artsobs, stk.ip, stk.pred$stk,
+                            Formula = formula4, mesh = Mesh$mesh, predictions = TRUE)
+#saveRDS(model_final, "R/output/model_final.RDS")
+saveRDS(model_final, "R/output/model_final_overdisp.RDS")
+message("Done and model saved!")
+
+

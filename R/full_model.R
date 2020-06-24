eb72d78a2876ef02936e6bbbf568d7df83df8df9
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-06-24 21:11:26 +0200

    Drop perimeter as covariate

--- R/full_model.R
+++ R/full_model.R
@@ -20,18 +20,18 @@ stk.artsobs <- stks$artsobs
 stk.ip <- stks$ip
 stk.pred <- stks$pred
 
-Use <- c("decimalLongitude","decimalLatitude", "log_area", "log_perimeter", 
+Use <- c("decimalLongitude","decimalLatitude", "log_area", 
          "log_catchment", "eurolst_bio10", "SCI")
 Use_CS <- c(Use, "distance_to_road", "HFP")
 
-#formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE, overdispersion = TRUE)
-formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE)
+formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE, overdispersion = FALSE)
+#formula4 <- MakeFormula(cov_names = Use_CS, second_sp_field = TRUE)
 
 message("Fitting model... \n")
 model_final <- FitModelTest(stk.survey, stk.artsobs, stk.ip, stk.pred$stk,
                             Formula = formula4, mesh = Mesh$mesh, predictions = TRUE)
 #saveRDS(model_final, "R/output/model_final.RDS")
-saveRDS(model_final, "R/output/model_final_overdisp.RDS")
+saveRDS(model_final, "R/output/model_final_2.RDS")
 message("Done and model saved!")
 
 

a5c0fe3804a1c0ae6e6f1833b01e66fc69feaeff
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-07-12 16:23:46 +0200

    changing name of hyperpars

--- R/Visualizing_species_comparison.R
+++ R/Visualizing_species_comparison.R
@@ -12,6 +12,7 @@ res_cv <- readRDS("R/output/cv_output_4modse0.RDS")
 
 library(patchwork)
 library(RColorBrewer)
+#library(ggtheme)
 
 
 # From "full_model.R":
@@ -39,8 +40,8 @@ Pred <- bind_rows(Trout = Pred_trout, Perch = Pred_perch,
 species.labs <- c("Brown trout", "European perch", "Arctic char", "Northern pike")  
 names(species.labs) <- c("Trout", "Perch", "Char", "Pike")
 
-limit_mean <- max(abs(Pred %>% filter(statistic == "mean") %>% select(value))) * c(-1, 1)
-limit_sd <- max(abs(Pred %>% filter(statistic == "stddev") %>% select(value))) * c(0, 1)
+limit_mean <- max(abs(Pred %>% filter(statistic == "mean") %>% dplyr::select(value))) * c(-1, 1)
+limit_sd <- max(abs(Pred %>% filter(statistic == "stddev") %>% dplyr::select(value))) * c(0, 1)
 
 mean_p <- ggplot(Pred %>% filter(statistic == "mean")) +
   geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
@@ -54,7 +55,6 @@ mean_p <- ggplot(Pred %>% filter(statistic == "mean")) +
         strip.background = element_blank(), strip.text.y = element_blank(),
         legend.title = element_blank()) +
   ggtitle("Mean")
-
 
 sd_p <- ggplot(Pred %>% filter(statistic == "stddev")) +
   geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
@@ -110,7 +110,7 @@ shared_all_sp <- ggplot(spat_fields %>% filter(statistic == "mean", field == "sh
   theme(axis.title = element_blank(), legend.position = "left",
         strip.background = element_blank(), strip.text.y = element_blank(),
         legend.title = element_blank()) +
-  ggtitle("First spatial field", subtitle = "Shared for both data sets")
+  ggtitle(expression(paste("First spatial field, ", xi[1]))) 
 
 
 bias_all_sp <- ggplot(spat_fields %>% filter(statistic == "mean", field == "bias")) +
@@ -124,151 +124,101 @@ bias_all_sp <- ggplot(spat_fields %>% filter(statistic == "mean", field == "bias
   theme_bw() +
   theme(axis.title = element_blank(), axis.ticks.y = element_blank(), 
         axis.text.y = element_blank(), legend.title = element_blank()) + 
-  ggtitle("Second spatial field", subtitle = "Citizen science data only")
+  ggtitle(expression(paste("Effort field, ", xi[2])))
 
 shared_all_sp + bias_all_sp
 
-ggsave("figs/spatial_fields_all_species.pdf", width = 6.6, height = 11)
+ggsave("figs/spatial_fields_all_species.pdf", width = 6.6, height = 10)
 
 # PLOTTING HYPERPARAMETERS ----
 
 hyperpar_df <- function(model){
-  shared <- dplyr::bind_rows(theta1 = as.data.frame(model$model$marginals.hyperpar[[1]]),
-                             theta2 = as.data.frame(model$model$marginals.hyperpar[[2]]),
-                             .id = "theta")
-  bias <- dplyr::bind_rows(theta1 = as.data.frame(model$model$marginals.hyperpar[[3]]),
-                           theta2 = as.data.frame(model$model$marginals.hyperpar[[4]]),
-                           .id = "theta")
+  shared <- dplyr::bind_rows(rho = as.data.frame(model$model$marginals.hyperpar[[1]]),
+                             sigma = as.data.frame(model$model$marginals.hyperpar[[2]]),
+                             .id = "hyperpar")
+  bias <- dplyr::bind_rows(rho = as.data.frame(model$model$marginals.hyperpar[[3]]),
+                           sigma = as.data.frame(model$model$marginals.hyperpar[[4]]),
+                           .id = "hyperpar")
   hyperpars <- dplyr::bind_rows(shared_field = shared, bias_field = bias, .id = "field")
   hyperpars
 }
 
-trout_theta <- hyperpar_df(model_trout)
-perch_theta <- hyperpar_df(model_perch)
-char_theta <- hyperpar_df(model_char)
-pike_theta <- hyperpar_df(model_pike)
+trout_hyperpars <- hyperpar_df(model_trout)
+perch_hyperpars <- hyperpar_df(model_perch)
+char_hyperpars <- hyperpar_df(model_char)
+pike_hyperpars <- hyperpar_df(model_pike)
 
-all_sp_hyperpar <- dplyr::bind_rows(trout = trout_theta, 
-                                    perch = perch_theta, 
-                                    char = char_theta, 
-                                    pike = pike_theta,
+all_sp_hyperpar <- dplyr::bind_rows(trout = trout_hyperpars, 
+                                    perch = perch_hyperpars, 
+                                    char = char_hyperpars, 
+                                    pike = pike_hyperpars,
                                     .id = "species") 
 
 
-theta1_shared <- ggplot(all_sp_hyperpar %>% filter(theta == "theta1", field == "shared_field")) + 
-  geom_line(aes(x = x, y = y, color = species), lwd = 0.8) +
-  ylab (expression(paste(pi, "(", theta, " | ", bold(y), ")"))) +
-  ggtitle(expression(paste(theta[1]^{}, " - shared field"))) +
-  xlim(0, 70) +
-  scale_color_brewer(palette = "Dark2") + 
+rho_shared <- ggplot(all_sp_hyperpar %>% filter(hyperpar == "rho", field == "shared_field")) + 
+  geom_line(aes(x = x, y = y, color = species, linetype = species), lwd = 0.8) +
+  ylab (expression(paste(pi, "(", rho, " | ", bold(y), ")"))) +
+  ggtitle(expression(paste(rho, " - shared field"))) +
+  xlim(0, 50) +
+  scale_color_viridis_d() +
   theme_bw() + theme(aspect.ratio=1, legend.position = "none", axis.title.x = element_blank(), 
                      plot.title = element_text(size = 11))
 
-theta2_shared <- ggplot(all_sp_hyperpar %>% filter(theta == "theta2", field == "shared_field")) + 
-  geom_line(aes(x = x, y = y, color = species), alpha = 0.8, lwd = 0.8) +
+sigma_shared <- ggplot(all_sp_hyperpar %>% filter(hyperpar == "sigma", field == "shared_field")) + 
+  geom_line(aes(x = x, y = y, color = species, linetype = species), lwd = 0.8) +
+  ylab (expression(paste(pi, "(", sigma, " | ", bold(y), ")"))) +
+  ggtitle(expression(paste(sigma, " - shared field"))) +
+  xlim(0, 0.9) +
+  scale_color_viridis_d() + 
+  theme_bw() + theme(aspect.ratio=1, legend.position = "none", axis.title.x = element_blank(), 
+                     plot.title = element_text(size = 11))
+
+rho_bias <- ggplot(all_sp_hyperpar %>% filter(hyperpar == "rho", field == "bias_field")) + 
+  geom_line(aes(x = x, y = y, color = species, linetype = species), lwd = 0.8) +
+  ylab("")+
+  ggtitle(expression(paste(rho, " - bias field"))) +
+  xlim(0, 50) +
+  scale_color_viridis_d()+ 
+  theme_bw() + theme(aspect.ratio=1, legend.position = "none", axis.title.x = element_blank(), 
+                     plot.title = element_text(size = 11))
+
+sigma_bias <- ggplot(all_sp_hyperpar %>% filter(hyperpar == "sigma", field == "bias_field")) + 
+  geom_line(aes(x = x, y = y, color = species, linetype = species), lwd = 0.8) +
   ylab ("") +
-  ggtitle(expression(paste(theta[2]^{}, " - shared field"))) +
-  xlim(0, 0.7) +
-  scale_color_brewer(palette = "Dark2") + 
-  theme_bw() + theme(aspect.ratio=1, legend.position = "none", axis.title.x = element_blank(), 
-                     plot.title = element_text(size = 11))
-
-theta1_bias <- ggplot(all_sp_hyperpar %>% filter(theta == "theta1", field == "bias_field")) + 
-  geom_line(aes(x = x, y = y, color = species), alpha = 0.8, lwd = 0.8) +
-  ylab (expression(paste(pi, "(", theta, " | ", bold(y), ")"))) +
-  ggtitle(expression(paste(theta[1]^{}, " - bias field"))) +
-  xlim(0, 70) +
-  scale_color_brewer(palette = "Dark2") + 
-  theme_bw() + theme(aspect.ratio=1, legend.position = "none", axis.title.x = element_blank(), 
-                     plot.title = element_text(size = 11))
-
-theta2_bias <- ggplot(all_sp_hyperpar %>% filter(theta == "theta2", field == "bias_field")) + 
-  geom_line(aes(x = x, y = y, color = species), alpha = 0.8, lwd = 0.8) +
-  ylab ("") +
-  ggtitle(expression(paste(theta[2]^{}, " - bias field"))) +
-  xlim(0, 0.7) +
-  scale_color_brewer(palette = "Dark2") + 
+  ggtitle(expression(paste(sigma, " - bias field"))) +
+  xlim(0, 0.9) +
+  scale_color_viridis_d() + 
   theme_bw() + theme(aspect.ratio=1, axis.title.x = element_blank(), 
                      plot.title = element_text(size = 11))
 
-
-((theta1_shared | theta2_shared)/(theta1_bias | theta2_bias))
+((rho_shared | rho_bias)/(sigma_shared | sigma_bias))
 
 ggsave("figs/thetas_all_species.pdf", width = 5, height = 4)
 
 
 
-# COMPARING DIFFERENT RANGES ----
 
-model_lowrange <- readRDS("R/output/model_trout_lowrange.RDS")
-model_10range <- readRDS("R/output/model_trout_10range.RDS")
-model_100range <- readRDS("R/output/model_trout_100range.RDS")
+# DOTS AND WHISKERS PLOTS ----
+Use <- c("decimalLongitude","decimalLatitude", "log_area", 
+         "log_catchment", "eurolst_bio10", "SCI")
+cov.labs <- c("Longitude", "Latitude", "Log area", "Log catchment", 
+              "Temperature", "Shoreline complexity index")
 
-spat_fields_lowrange <- make_random_field_df(model_lowrange, sp_polygon = norway.poly, 
-                                          mesh = Mesh$mesh)
-spat_fields_10range <- make_random_field_df(model_10range, sp_polygon = norway.poly, 
-                                          mesh = Mesh$mesh)
-spat_fields_100range <- make_random_field_df(model_100range, sp_polygon = norway.poly, 
-                                         mesh = Mesh$mesh)
+dots_whiskers_inla(model_trout) + scale_y_discrete(labels = cov.labs) +
+  ggtitle(paste0("Brown trout"))
+ggsave(paste0("figs/coefficient_plot_", "trout", ".pdf"), height = 2.5, width = 5) 
 
-spat_fields <- bind_rows(lowrange = spat_fields_lowrange, midrange = spat_fields_10range, 
-                         highrange = spat_fields_100range, .id = "range")
+dots_whiskers_inla(model_perch) + scale_y_discrete(labels = cov.labs) +
+  ggtitle(paste0("European perch"))
+ggsave(paste0("figs/coefficient_plot_", "perch", ".pdf"), height = 2.5, width = 5)
 
+dots_whiskers_inla(model_char) + scale_y_discrete(labels = cov.labs) +
+  ggtitle(paste0("Arctic char"))
+ggsave(paste0("figs/coefficient_plot_", "char", ".pdf"), height = 2.5, width = 5)
 
-shared_all_sp <- ggplot(spat_fields %>% filter(statistic == "mean", field == "shared")) +
-  geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
-  scale_fill_viridis(option = "magma", direction = -1) +
-  facet_grid(rows = vars(range)) +
-  geom_polygon(data = norway, aes(long, lat, group = group), 
-               color="black", fill = NA) + coord_quickmap() + 
-  theme_bw() +
-  theme(axis.title = element_blank(), legend.position = "left",
-        strip.background = element_blank(), strip.text.y = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle("First spatial field", subtitle = "Shared for both data sets")
+dots_whiskers_inla(model_pike) + scale_y_discrete(labels = cov.labs) +
+  ggtitle(paste0("Northern pike"))
+ggsave(paste0("figs/coefficient_plot_", "pike", ".pdf"), height = 2.5, width = 5)
 
-
-bias_all_sp <- ggplot(spat_fields %>% filter(statistic == "sd", field == "shared")) +
-  geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
-  scale_fill_viridis(option = "viridis", direction = -1) +
-  facet_grid(rows = vars(range)) +
-  geom_polygon(data = norway, aes(long, lat, group = group), 
-               color="black", fill = NA) + coord_quickmap() + 
-  theme_bw() +
-  theme(axis.title = element_blank(), axis.ticks.y = element_blank(), 
-        axis.text.y = element_blank(), legend.title = element_blank()) + 
-  ggtitle("first spatial field", subtitle = "standard deviation")
-
-shared_all_sp + bias_all_sp & theme(plot.margin = margin())
-
-
-# ----
-Pred <- prediction_df(stk.pred = stk.pred, model = model_100range)
-Pred_mean <- Pred %>% filter(statistic=="mean")
-Pred_sd <- Pred %>% filter(statistic=="stddev")
-limit_mean <- max(abs(Pred_mean$value)) * c(-1, 1)
-limit_sd <- max(abs(Pred_sd$value)) * c(0, 1)
-
-p_mean <- ggplot(Pred_mean) +
-  geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
-  scale_fill_continuous_diverging(palette = "Purple-Green", limit = limit_mean)  +
-  geom_polygon(data = norway, aes(long, lat, group = group), 
-               color='black', fill = NA) + coord_quickmap() +
-  ggtitle(label = "Mean") +
-  labs(fill = element_blank()) + 
-  theme_bw() + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5))
-
-p_sd <- ggplot(Pred_sd) +
-  geom_raster(aes(x = decimalLongitude, y = decimalLatitude, fill = value)) +
-  scale_fill_continuous_diverging(palette = "Purple-Green", limit = limit_sd)  +
-  geom_polygon(data = norway, aes(long, lat, group = group), 
-               color='black', fill = NA) + coord_quickmap() + 
-  ggtitle(label = "Standard deviation") +
-  labs(fill = element_blank()) +
-  theme_bw() + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5))
-
-p_post <- ggarrange(p_mean, p_sd)
-annotate_figure(p_post, top = text_grob(paste0(full_name, ": Predicted posterior log intensity"), 
-                                        size = 14))
 
 

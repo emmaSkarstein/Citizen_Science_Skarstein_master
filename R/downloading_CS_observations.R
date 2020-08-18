a7146a5237f7ae712c4facd92d9b27d962c67448
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-08-18 11:10:46 +0200

    Commented out Sweden stuff

--- R/downloading_CS_observations.R
+++ R/downloading_CS_observations.R
@@ -4,7 +4,8 @@
 
 
 source('R/Download_lakefish_functions.R')
-fish_names <- readRDS('fish_names.rds')
+#fish_names <- readRDS('fish_names.rds')
+fish_names <- c("Salmo trutta", "Perca fluviatilis", "Salvelinus alpinus", "Esox lucius")
 
 #setwd(paste0(getwd(), "/Norwegian_Species_Observation_Service"))
 
@@ -15,7 +16,7 @@ download_lakefish_dataset(fish_names, n_try = 100, file_marker = 'Norge',
 # Obs: This downloads the same species for Sweden. 
 # The species list is based on the most common NORWEGIAN fish.
 
-download_lakefish_dataset(fish_names, n_try = 100, file_marker = 'Sverige', 
-                          key_dataset = '38b4c89f-584c-41bb-bd8f-cd1def33e92f',  
-                          key_tax = NA)
+#download_lakefish_dataset(fish_names, n_try = 100, file_marker = 'Sverige', 
+#                          key_dataset = '38b4c89f-584c-41bb-bd8f-cd1def33e92f',  
+#                          key_tax = NA)
 

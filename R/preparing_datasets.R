851e842264bc26f7f5ad4ec6ab649b9cbabde259
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-08-18 11:38:15 +0200

    Commented out the transcribed gillnet, NORS and PIKE stuff, since that isn't used

--- R/preparing_datasets.R
+++ R/preparing_datasets.R
@@ -8,11 +8,11 @@ lakes <- readRDS("data/Norwegian_lakes.rds")
 
 #---------------------------------------------------------------------------
 # NORS
-merge_occ_event(file_path = "NORS/data", file_type = "csv", sep = ",")
+#merge_occ_event(file_path = "NORS/data", file_type = "csv", sep = ",")
 
 #---------------------------------------------------------------------------
 # PIKE
-merge_occ_event(file_path = "PIKE/data", file_type = "txt", quote = "")
+#merge_occ_event(file_path = "PIKE/data", file_type = "txt", quote = "")
 
 #---------------------------------------------------------------------------
 # Kautokeino
@@ -29,8 +29,8 @@ merge_occ_event(file_path = "Fish_status_survey_of_nordic_lakes/data",
 
 #---------------------------------------------------------------------------
 # Transcriptions of gillnet test-fishing
-merge_occ_event(file_path = "Transcribed_gillnet_test_fishing_data_norway/data", 
-                file_type = "txt")
+#merge_occ_event(file_path = "Transcribed_gillnet_test_fishing_data_norway/data", 
+#                file_type = "txt")
 
 #---------------------------------------------------------------------------
 # Citizen science observations
@@ -38,17 +38,16 @@ merge_occ_event(file_path = "Transcribed_gillnet_test_fishing_data_norway/data",
 
 
 # CLEANING DATA
-
 survey_dirty <- readRDS("Fish_status_survey_of_nordic_lakes/data/merged.rds")
-transc_gn_dirty <- readRDS("Transcribed_gillnet_test_fishing_data_norway/data/merged.rds")
+#transc_gn_dirty <- readRDS("Transcribed_gillnet_test_fishing_data_norway/data/merged.rds")
 artsobs_dirty <- readRDS("Nordic_Species_Observation_Services/data/GBIF_download_Norge.rds")
 
 
 survey <- data_prep(survey_dirty, lakes)
 saveRDS(survey, "Fish_status_survey_of_nordic_lakes/data/clean.rds")
 
-transc_gn <- data_prep(transc_gn_dirty, lakes)
-saveRDS(transc_gn, "Transcribed_gillnet_test_fishing_data_norway/data/clean.rds")
+#transc_gn <- data_prep(transc_gn_dirty, lakes)
+#saveRDS(transc_gn, "Transcribed_gillnet_test_fishing_data_norway/data/clean.rds")
 
 artsobs <- data_prep(artsobs_dirty, lakes, max_dist = 30)                         
 saveRDS(artsobs, "Nordic_Species_Observation_Services/data/clean.rds")
